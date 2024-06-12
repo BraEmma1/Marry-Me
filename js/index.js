f73a568ec5d00ec0a947aae8cd06ef2b6455974b
larem <emmanuelosei831@gmail.com>
2024-06-12 09:56:11 +0000

    first commit for marry Me

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn-btn');
+btn.onclick =function(){
+    alert('button has been clicked')
+}
+const headerbtn = document.getElementById('header_btn');
+headerbtn.onclick = function()
+{
+    alert('Get Ready To Get Marry');
+}
